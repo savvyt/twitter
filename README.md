23b0472e8d51ae55132766e5ca700c31d94becd5
sam <sam@toposs-mbp.home>
2021-01-25 13:08:20 -0500

    code cleaanup

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ https://github.com/TDehaene/blogposts/tree/master/got_sentiment
 * Setup advanced packaging tool, checked for Python 3, and installed pip using the instructions [here](https://www.digitalocean.com/community/tutorials/how-to-install-python-3-and-set-up-a-programming-environment-on-debian-10)
 * Script requires tweepy and Google secretmanager, so I installed those packages
 	* Ran into a problem where secretmanager wouldn't finish installing (see link [here](https://github.com/grpc/grpc/issues/22815)). Once I upgraded pip (with "pip3 install --upgrade pip") the install finished quickly
+# Also installed git with `sudo apt install git`
 
 ## Installed packages for Python script
 * pip3 install tweepy
--- stream-to-pubsub.py
+++ stream-to-pubsub.py
@@ -35,29 +35,8 @@ topic_path = publisher.topic_path(f"{project_id}", f"{pub_sub_topic}")
 # ???
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=False)
 
-# # Method to push messages to Pub/Sub
-# def write_to_pubsub(data):
-#     try:
-#         if data["lang"] == "en":     
-
-#             # # V1 for sending messages to Pub/Sub
-#             # publisher.publish(topic_path, data=json.dumps({
-#             #     "id": data["id_str"],
-#             #     "posted_at": data["created_at"],
-#             #     "text": data["text"]
-#             # }).encode("utf-8"), tweet_id=str(data["id_str"]).encode("utf-8"))            
-
-#             # Dump everything
-#             v2 = json.dumps(data).encode("utf-8")
-
-#             publisher.publish(topic_path, data=v2, \
-#                 tweet_id=str(data["id_str"]).encode("utf-8"))            
-
-#     except Exception as e:
-#         raise
-
 # Method to push messages to Pub/Sub
-def write_to_pubsub_v2(tweet):
+def write_to_pubsub(tweet):
 
     str_keys = ['created_at','id_str','in_reply_to_status_id_str',\
         'in_reply_to_user_id_str','lang','text']
@@ -65,17 +44,12 @@ def write_to_pubsub_v2(tweet):
     dict_keys = ['coordinates','place','entities','user']
     bool_keys = ['truncated']
 
-    # keys_dict = {'str': {'null_type': 'none', \
-    #     'vals': ['created_at','id_str','in_reply_to_status_id_str',\
-    #     'in_reply_to_user_id_str','lang','text']},
-    #     'bool': {'null_type': 'none', \
-    #     'vals': ['truncated']},
-
-    #     }
-
     processed_doc = {}
 
     try:
+        time_formatted = datetime.strptime(tweet['created_at'], "%a %b %d %H:%M:%S +0000 %Y")
+        print(time_formatted)
+        
         for key in str_keys:
             if key in tweet:
                 processed_doc[key] = tweet[key]
@@ -103,130 +77,18 @@ def write_to_pubsub_v2(tweet):
     except Exception as e:
         print("Dict processing failed")
         print(e)
-        raise                  
-
-    # # if processed_doc['coordinates'] != "null":
-    # print("processed_doc:")
-    # print(processed_doc)
-    # print()   
+        raise                   
      
     try:
-        if processed_doc["lang"] == "en":     
-
-            # # V1 for sending messages to Pub/Sub
-            # publisher.publish(topic_path, data=json.dumps({
-            #     "id": data["id_str"],
-            #     "posted_at": data["created_at"],
-            #     "text": data["text"]
-            # }).encode("utf-8"), tweet_id=str(data["id_str"]).encode("utf-8"))            
-
-            # Dump everything
-            publisher.publish(topic_path, \
-                data=json.dumps(processed_doc).encode("utf-8"), \
-                tweet_id=str(processed_doc["id_str"]).encode("utf-8"))            
+        # Publish data
+        publisher.publish(topic_path, \
+            data=json.dumps(processed_doc).encode("utf-8"), \
+            tweet_id=str(processed_doc["id_str"]).encode("utf-8"))            
 
     except Exception as e:
         print("Publishing step failed")
         print(e)
         raise        
-
-# # Method to format a tweet from tweepy
-# def reformat_tweet(tweet):
-#     # x = tweet
-
-#     # processed_doc = {
-#     #     "id": x["id"],
-#     #     "lang": x["lang"],
-#         # "retweeted_id": x["retweeted_status"]["id"] if "retweeted_status" in x else None,
-#         # "favorite_count": x["favorite_count"] if "favorite_count" in x else 0,
-#         # "retweet_count": x["retweet_count"] if "retweet_count" in x else 0,
-#         # "lat": x["coordinates"]["coordinates"][0] if x["coordinates"] else 0,
-#         # "lng": x["coordinates"]["coordinates"][1] if x["coordinates"] else 0,
-#         # "place": x["place"]["country_code"] if x["place"] else None,
-#         # "user_id": x["user"]["id"],
-#         # "created_at": time.mktime(time.strptime(x["created_at"], "%a %b %d %H:%M:%S +0000 %Y"))
-#     # }
-
-#     # if x["entities"]["hashtags"]:
-#     #     processed_doc["hashtags"] = [{"text": y["text"], "startindex": y["indices"][0]} for y in
-#     #                                  x["entities"]["hashtags"]]
-#     # else:
-#     #     processed_doc["hashtags"] = []
-
-#     # if x["entities"]["user_mentions"]:
-#     #     processed_doc["usermentions"] = [{"screen_name": y["screen_name"], "startindex": y["indices"][0]} for y in
-#     #                                      x["entities"]["user_mentions"]]
-#     # else:
-#     #     processed_doc["usermentions"] = []
-
-#     # if "extended_tweet" in x:
-#     #     processed_doc["text"] = x["extended_tweet"]["full_text"]
-#     # elif "full_text" in x:
-#     #     processed_doc["text"] = x["full_text"]
-#     # else:
-#     #     processed_doc["text"] = x["text"]
-
-#     str_keys = ['created_at','id_str','in_reply_to_status_id_str',\
-#         'in_reply_to_user_id_str','lang','text']
-#     bool_keys = ['truncated']
-#     dict_keys = ['coordinates','place','entities','user']
-#     int_keys = ['quote_count','reply_count','retweet_count','favorite_count']
-
-#     # keys_dict = {'str': {'null_type': 'none', \
-#     #     'vals': ['created_at','id_str','in_reply_to_status_id_str',\
-#     #     'in_reply_to_user_id_str','lang','text']},
-#     #     'bool': {'null_type': 'none', \
-#     #     'vals': ['truncated']},
-
-#     #     }
-
-#     processed_doc = {}
-
-#     for key in str_keys:
-#         if key in tweet:
-#             processed_doc[key] = tweet[key]
-#         else:
-#             processed_doc[key] = "None"        
-
-#     for key in int_keys:
-#         if key in tweet:
-#             processed_doc[key] = tweet[key]
-#         else:
-#             processed_doc[key] = 0
-
-#     for key in dict_keys:
-#         if key in tweet:        
-#             processed_doc[key] = json.dumps(tweet[key])
-#         else:
-#             processed_doc[key] = "None"
-
-#     # if processed_doc['coordinates'] != "null":
-#     print("processed_doc:")
-#     print(processed_doc)
-#     print()
-
-#     # # try:
-#     # if x['place']:
-#     #     print('created_at: ' + x['created_at'])
-#     #     print('id_str: ' + x['id_str'])
-#     #     print('text: ' + x['text'])    
-#     #     print('lang: ' + x['lang'])
-#     #     print('place: ' + x['place'])            
-#     #     print('coordinates: ' + x['coordinates'])
-#     #     print('retweeted_status: ' + x['retweeted_status'])
-#     #     print('quote_count: ' + x['quote_count'])
-#     #     print('reply_count: ' + x['reply_count'])
-#     #     print('retweet_count: ' + x['retweet_count'])
-#     #     print('favorite_count: ' + x['favorite_count'])
-#     #     print('entities: ' + x['entities'])
-#     #     print('truncated: ' + x['truncated'])
-#     #     print('in_reply_to_status_id_str: ' + x['in_reply_to_status_id_str'])
-#     #     print('in_reply_to_user_id_str: ' + x['in_reply_to_user_id_str'])            
-#     #     print()
-#     # # except:
-#     # #     pass        
-    
-#     return processed_doc
 
 # Custom listener class
 class StdOutListener(StreamListener):
@@ -236,23 +98,23 @@ class StdOutListener(StreamListener):
 
     def __init__(self):
         super(StdOutListener, self).__init__()
-        self._counter = 0
+        # self._counter = 0
 
     def on_status(self, data):
-        self._counter += 1
-        write_to_pubsub_v2(data._json)
-        # write_to_pubsub(reformat_tweet(data._json))
+        # self._counter += 1
+        write_to_pubsub(data._json)
         return True
 
     def on_error(self, status):
-        print("found an error")
-        print("status: ", status)
-        if status == 420:
-            print("rate limit active")
-            return False
+        print("Error found!")
+        print("Status:")
+        print(status)
+        # if status == 420:
+        #     print("Hitting rate limit")
+        #     return False
 
 # Start listening
 print("Listening for tweets now")
 l = StdOutListener()
-stream = tweepy.Stream(auth, l, tweet_mode="extended", is_async=True)
+stream = tweepy.Stream(auth, l, tweet_mode="extended", is_async=True) # add 'is_async=True' so your connection isn't broken
 stream.filter(track=search_terms)
