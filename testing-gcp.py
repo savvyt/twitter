67d4c28ebfd734bda096e4918a3ec8504e1fc058
sam <sam@toposs-mbp.home>
2021-01-26 10:54:16 -0500

    debugging

--- testing-gcp.py
+++ testing-gcp.py
@@ -9,7 +9,10 @@ c = ntplib.NTPClient()
 # Provide the respective ntp server ip in below function
 response = c.request('uk.pool.ntp.org', version=3)
 response.offset
+print()
+print("Time check!")
 print(datetime.fromtimestamp(response.tx_time, timezone.utc))    
+print()
 
 # Enter your GCP project info here
 project_id = "twitter-296505"
@@ -23,9 +26,6 @@ for secret_name in secret_names:
     secret_dict[secret_name] = secret_client.access_secret_version({"name": \
         f"projects/{project_number}/secrets/{secret_name}/versions/latest"}).payload.data.decode("UTF-8")
 
-print("secret_dict")
-print(secret_dict)
-
 # Authenticate to the Twitter API
 auth = tweepy.OAuthHandler(secret_dict["twitter-api-key"], \
     secret_dict["twitter-api-secret"])
@@ -34,14 +34,16 @@ auth.set_access_token(secret_dict["twitter-access-token"], \
 
 api = tweepy.API(auth)
 
-print("Performing simple test!")
+print("Simple batch API call")
+print()
 # Simple test
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
-
 print()
-print("Performing streaming test!")
+
+print("Streaming API call test")
+print()
 # Streaming test
 class MyStreamListener(tweepy.StreamListener):
 
